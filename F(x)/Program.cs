44f96b0be211caf5791dead37f64bc3ab2dec428
sigmabigdata <sigmabigdata@gmail.com>
2023-01-05 13:31:41 +0300

    F(x)

---
+++ F(x)/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 13;
+int c1 = 15;
+int a2 = 22;
+int b2 = 334;
+int c2 = 36;
+int a3 = 15;
+int b3 = 111;
+int c3 = 14;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
