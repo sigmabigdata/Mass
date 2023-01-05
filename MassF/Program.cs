0acd7f6b170f32d7cefc9cd7ae6a70ed0b770f5e
sigmabigdata <sigmabigdata@gmail.com>
2023-01-05 13:53:15 +0300

    MassF

---
+++ MassF/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {22, 66, 65, 54, 58, 44, 26, 87, 65, 888};
+
+int n = array.Length;
+int find = 65;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index = index + 1;
+}
